29e9add21a86fd028387accba012a4a380d5b533
mmetcalfputnam <mmetcalfputnam@Hy-Vee.com>
2017-06-30 09:53:23 -0500

    Added test class/method

---
+++ Formulas/FormulasTests/Class1Tests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Formulas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formulas.Tests {
+    [TestClass()]
+    public class Class1Tests {
+        [TestMethod()]
+        public void testThisTest() {
+            var c1 = new Class1();
+
+            Assert.AreEqual(5, c1.testThis());
+        }
+    }
+}
