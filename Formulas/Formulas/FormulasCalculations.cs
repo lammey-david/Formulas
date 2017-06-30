758c28fe9f99c343aefd9e4e17e1a5810ac504c7
wittman-timothy <wittman.timothy@gmail.com>
2017-06-30 14:22:30 -0500

    Initial Test Class Set-up with Moq & Repository

---
+++ Formulas/Formulas/FormulasCalculations.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formulas
+{
+    public class FormulasCalculations
+    {
+        private readonly FormulasRepository _repository;
+
+        public FormulasCalculations(FormulasRepository repo)
+        {
+            _repository = repo;
+        }
+
+        public IEnumerable<Formulas> GetTotal(double amt)
+        {
+            return _repository.GetTotal().Where(formulas => formulas.Total == amt);
+        }
+    }
+}
